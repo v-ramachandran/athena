70f1c17339f564b92e6a8a226fbb8e57cae4a857
v-ramachandran <v.ram28@gmail.com>
2014-11-28 05:29:08 -0500

    adding property retrieval js

---
+++ app/assets/javascripts/mturk_property_manager.js
@@ -0,0 +1,47 @@
+var MTurk = MTurk || {}
+MTurk.WorkerPropertyManager = function(form_selector) {
+
+  var TARGET = $(form_selector);
+  var INELIGIBLE_ASSIGNMENT_NAME = "ASSIGNMENT_ID_NOT_AVAILABLE";
+  var ASSIGNMENT_ID_NAME = "assignmentId";
+  var WORKER_ID_NAME = "workerId";
+  var HIT_ID = "hitId";
+
+  /**
+   * This method Gets URL Parameters (GUP).
+   * Method provided in the MTurk Samples and reproduced here.
+   **/
+  var get_url_parameter_value = function(name)
+  {
+    var regexS = "[\\?&]"+name+"=([^&#]*)";
+    var regex = new RegExp( regexS );
+    var tmpURL = window.location.href;
+    var results = regex.exec( tmpURL );
+    if( results == null )
+      return "";
+    else
+      return results[1];
+  }
+
+  var populate_task_properties = function() {
+    var assignment_id = get_url_parameter_value(ASSIGNMENT_ID_NAME);
+    var hit_id = get_url_parameter_value(HIT_ID_NAME);
+    var worker_id = get_url_parameter_value(WORKER_ID_NAME);
+
+    TARGET.find(".assignment_id_field").val(assignment_id);
+    TARGET.find(".hit_id_field").val(assignment_id);
+    TARGET.find(".worker_id_field").val(assignment_id);
+  }
+
+  var validate_submission_eligibility = function() {
+    var assignment_id = get_url_parameter_value(ASSIGNMENT_ID_NAME);
+    if (assignment_id == INELIGIBLE_ASSIGNMENT_NAME) {
+      TARGET.find(".submit_button").prop('disabled', true);
+    }
+  }
+
+  return {
+    validate_submission_eligibility : validate_submission_eligibility,
+    populate_task_properties : populate_task_properties
+  }
+};
